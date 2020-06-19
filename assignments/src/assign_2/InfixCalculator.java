db0648607d53c95c9fe2a8a426ec68cff5f6c32d
sabathrodriguez <sabathcasillas1@gmail.com>
2020-06-18 20:04:10 -0600

    first commit

---
+++ assignments/src/assign_2/InfixCalculator.java
@@ -0,0 +1,24 @@
+package assign_2;
+
+import java.util.Stack;
+
+public class InfixCalculator {
+    public static void main(String[] args) {
+
+    }
+
+    public static int evaluate(String infixString) {
+
+        return -1;
+    }
+
+    private static String infixToPostfix(String input) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if ()
+        }
+
+        return "";
+    }
+}
